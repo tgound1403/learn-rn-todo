c559399bebb5f80aa69ea429d623747b55dcb639
tgound1403 <nguyentrieuduong14032002@gmail.com>
2025-06-25 23:11:44 +0700

    feat: add use firebase

--- app/home.tsx
+++ app/home.tsx
@@ -85,7 +85,11 @@ const HomeScreen = () => {
   | `componentDidUpdate`   | `useEffect(() => {}, [deps])`        |
   | `componentWillUnmount` | `return () => {}` inside `useEffect` |
   */
+
+  const { CalendarModule } = NativeModules;
+  
   useEffect(() => {
+    CalendarModule.createCalendarEvent('testName', 'testLocation');
     getContacts().then((contacts) => console.log(contacts))
   .catch((e) => console.error(e));
   }, []);
@@ -115,10 +119,7 @@ const HomeScreen = () => {
   const handleCancel= () => {
     formDispatch({ type: "RESET" });
     setModalVisibility(!modalVisibility);
-    // CalendarModule.createCalendarEvent('testName', 'testLocation');
   }
-
-  const { CalendarModule } = NativeModules;
 
   return (
     <SafeAreaProvider>
